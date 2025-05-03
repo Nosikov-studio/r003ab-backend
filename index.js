897d657b079a668331c964d0fc9ea37b0df0e86a
Nos <zvezdazanatih@gmail.com>
2025-05-03 14:18:18 +0700

    1

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require ('express');
+const querystring = require('querystring');
+const mysql2 = require ('mysql2/promise');
+const pool = mysql2.createPool ({
+    host: 'localhost',
+    user: 'root',
+    database: 'expo',
+    password: 'password',
+});
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', function (req, res) {
+    pool.query('SELECT * FROM tab1').then(function(data) {
+        res.json(data);
+    });
+});
+app.post('/add', async function (req, res) {
+    console.log(req.body);
+    const {nam, ag} = req.body;
+    await pool.query('INSERT INTO tab1 SET ?',{
+        name:nam,
+        age:Number(ag)
+    });
+   res.redirect('http://truruki.ru/');
+});
+app.listen(30333, ()=> {
+    console.log("It's started", new Date());
+});
